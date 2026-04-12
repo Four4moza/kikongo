d40c3ff2a311f904b2ec92a5aea1791584449743
formoza <formozamanuel1705@gmail.com>
2026-04-12 15:31:41 +0100

    main feito

---
+++ FRONTEND/src/pages/Admin/Admin.jsx
@@ -0,0 +1,3 @@
+export default function Admin() {
+  return <h1>Página Admin</h1>;
+}
---
+++ FRONTEND/src/pages/Main/Main.jsx
@@ -0,0 +1,57 @@
+import "./Main.css"
+import { Link } from "react-router-dom"
+
+function Main() {
+  return (
+    <div className="main">
+
+      
+      <header className="navbar">
+
+        <div className="logo">
+          <span>Mwana</span>
+        </div>
+
+        <div className="nav-buttons">
+
+          <Link to="">
+            <button className="btn-login">
+              entrar
+            </button>
+          </Link>
+
+          <Link to="">
+            <button className="btn-signup">
+              cadastrar
+            </button>
+          </Link>
+
+        </div>
+
+      </header>
+
+
+      <div className="hero">
+
+        <h1>
+          Aprenda a falar <span>kikongo</span>
+        </h1>
+
+        <p>
+          Conecte-se com as suas raízes Bakongo através do Kikongo,
+          preserve e valorize a cultura Angolana.
+        </p>
+
+        <Link to="/lesson">
+          <button className="btn-start">
+            começar →
+          </button>
+        </Link>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Main
